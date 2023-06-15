bcd487850634ebba0ab780d5f8f5f7f813389c69
riversun.org@gmail.com <riversun.org@gmail.com>
2023-06-15 20:28:13 +0900

    update comments

--- fastsession/fast_session_middleware.py
+++ fastsession/fast_session_middleware.py
@@ -32,8 +32,9 @@ class FastSession:
 class FastSessionMiddleware(BaseHTTPMiddleware):
     """
     A FastAPI middleware for managing user sessions.
-    FastAPIのユーザーセッションを管理するためのミドルウェア。
     """
+
+    # FastAPIのユーザーセッションを管理するためのミドルウェア。
 
     def __init__(self, app,
                  secret_key,  # クッキー署名用のキー
@@ -45,7 +46,6 @@ class FastSessionMiddleware(BaseHTTPMiddleware):
                  session_object="session",  # request.state以下にぶるさげるSessionオブジェクトの属性名
                  logger=None):
         """
-        Initialize FastSessionMiddleware.
         FastSessionMiddlewareを初期化する。
         """
         super().__init__(app)
@@ -67,12 +67,22 @@ class FastSessionMiddleware(BaseHTTPMiddleware):
     def create_session_cookie(self, session_id):
         """
         Create and sign a session cookie.
-        セッションキーを署名してクッキーに保存。
         """
+
+        # セッションID に署名してクッキーオブジェクトに保存する
+
+        # たとえば、セッションクッキーの名前が "session" とするとき、
+        # 　{"session":セッションID} な　「セッションID入り辞書オブジェクト」 を作り、
+        # その　セッションID入り辞書オブジェクトに対して署名を行う
         session_id_dict_obj = {self.session_cookie_name: session_id}
+
+        # 「セッションID入り辞書オブジェクト」 に署名をしたものは「署名済セッションID文字列」と呼ぶこととする。
+        # 辞書オブジェクトがシリアライズされてるので「署名済セッションID入り辞書オブジェクト」ではなく「署名済セッションID文字列」とする。
         signed_session_id = self.serializer.encode(session_id_dict_obj)  # ser.dumps({'session_id': session_id})
 
         cookie = SimpleCookie()
+
+        # クッキーオブジェクトにキーを "session" として、今署名済の　「署名済セッションID文字列」　を格納する
         cookie[self.session_cookie_name] = signed_session_id
 
         if self.http_only:
@@ -91,39 +101,39 @@ class FastSessionMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         """
         Dispatch the request, handling session management.
-        リクエストをディスパッチし、セッション管理を行う。
         """
 
-        # Get session key from cookies. クッキーからセッションキーを取得
+        # リクエストをディスパッチし、セッション管理を行う。
+        # クライアントからのリクエストに含まれるクッキーから
+        # 「署名済セッションID文字列」を取得する
         signed_session_id = request.cookies.get(self.session_cookie_name)
 
         cookie = None
+
         if signed_session_id is None:
-            # No session cookie, completely new access.
-            # => Generate a new session.
             # セッションクッキーが無い完全新規アクセス
             # => セッションの新規生成
             self.logi(f"Completely new access with no session cookies")
+
+            # セッションID に署名してクッキーオブジェクトに保存する。
+            # また request.state 以下にセッションマネージャをぶるさげてセッションの入出力ができるようにする
             cookie = await self.create_new_session_id_and_store(request, cause="new")
 
         else:
-            # Access with session cookie. セッションクッキーがある状態でアクセス
+            # セッションクッキーがある状態でアクセス
+
+            # 「署名済セッションID文字列」をデコードして「セッションID入り辞書オブジェクト」を得る
             decoded_dict, err = self.serializer.decode(signed_session_id)
 
             if decoded_dict is not None:
-                #
-                # Cookie signature verification succeeded.
-                # クッキー署名検証に成功
+
+                # - クッキー署名検証に成功したとき
+
+                # 「セッションID入り辞書オブジェクト」から セッションID を取得する
                 session_id = decoded_dict.get(self.session_cookie_name)
                 session_store = self.session_store.get_store(session_id)
 
                 if session_store is None:
-                    # The session cookie is correct and the session ID decoded from it is also normal.
-                    # However, the session store linked to the session ID could not be properly retrieved.
-                    # This happens when the server is restarted and the in-memory store is lost,
-                    # but the session cookie remains in the user's browser.
-                    # => Regenerate the session ID and the store.
-
                     # 正しい署名のクッキーがあり、そこからデコードしたセッションIDも正常
                     # だがセッションIDにひもづいたセッションストアが正しく取得できなかった
                     # こうなる原因はサーバーを再起動しオンメモリのストアが消えたがユーザーの
@@ -134,8 +144,6 @@ class FastSessionMiddleware(BaseHTTPMiddleware):
                     cookie = await self.create_new_session_id_and_store(request, cause="valid_cookie_but_no_store")
                     # request.state.session["__cause__"] =
                 else:
-                    # The session cookie is correct, the session ID decoded from it is also normal,
-                    # and the session store linked to the session ID was properly retrieved.
 
                     # 正しい署名のクッキーがあり、そこからデコードしたセッションIDも正常
                     # かつセッションIDにひもづいたセッションストアが正しく取得できた
@@ -153,13 +161,6 @@ class FastSessionMiddleware(BaseHTTPMiddleware):
                     session_store["__cause__"] = "success"
 
             else:
-                # Cookie signature verification failed.
-                # Signature verification of the session id failed due to some reason.
-                # Reason 1: Tampering with the session id.
-                # Reason 2: Expired.
-                # => Delete the storage for the old session ID.
-                # => Generate a new session.
-
                 # クッキーの署名検証に失敗
                 # 理由１　セッションidの改ざん
                 # 理由２　有効期限切れ
@@ -168,33 +169,29 @@ class FastSessionMiddleware(BaseHTTPMiddleware):
                 self.logi(f"Session cookies available. Verification failed! err:{err}")
 
                 if err == "SignatureExpired":
-                    # In case the session has expired.
-                    # セッションの有効期限が
-                    # Expired. セッションの有効期限が切れていた場合
+                    # セッションの有効期限が切れていた場合
                     pass
 
                 cookie = await self.create_new_session_id_and_store(request, cause=f"renew after {err}")
 
         response = await call_next(request)
 
-        # From here, processing for the response side.
         # ここから response 側の処理
         if cookie is not None:
-            # If there is a cookie to be set.
-            # => Set a cookie that encodes the session_id.
-
-            # セットすべきクッキーがある場合
+            # - セットすべきクッキーがあるとき
             # => session_id をエンコードしたクッキーをセットする
+
             cookie_val = cookie.output(header="").strip()
-            response.headers["Set-Cookie"] = cookie_val
+            response.headers["Set-Cookie"] = cookie_val  # レスポンスヘッダにセッションID署名済データが入ったクッキーをセットし、クライアント側に反映する
 
         return response
 
     async def create_new_session_id_and_store(self, request, cause=None):
         """
         Create a new session ID and its corresponding store.
-        新しいセッションIDとそのストアを生成する。
         """
+
+        # セッションID に署名してクッキーオブジェクトに保存する。また request.state 以下にセッションマネージャをぶるさげてセッションの入出力ができるようにする
         session_id = str(uuid.uuid4())
         session_store = self.session_store.create_store(session_id)
 
